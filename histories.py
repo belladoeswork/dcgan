897c973f50fd3ea1e1b16d2ef8b9d2a1a304aef7
belladoeswork <belladoeswork@gmail.com>
2024-02-26 06:11:16 +0800

    adjustments to pDB

--- histories.py
+++ histories.py
@@ -1,6 +1,46 @@
-H1 = ['horTh', 'age', 'menostat', 'tsize', 'tgrade', 'pnodes', 'progrec', 'estrec', 'time', 'cens', 'hormon', 'meno', 'surgery', 'radTh']
-A1 = 'Decision 1 Hormone Therapy (Y/N)'
-S2 = ['Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)',
+H1 = ['age',
+      'ERBB2_CPN_amplified',
+      'race',
+      'menopausal_status', 
+
+      
+      'tumor_stage_preTrt', 'nuclear_grade_preTrt', 'metastasis_stage_preTrt', 'ER_preTrt', 'ER_percentage_preTrt', 'ER_expr_preTrt', 'ER_fmolmg_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt', 'Erbeta_preTrt', 'PR_preTrt', 'PR_percentage_preTrt', 'PR_expr_preTrt', 'PR_fmolmg_preTrt', 'HER2_preTrt', 'HER2_IHC_score_preTrt', 'HER2_expr_preTrt', 'HER2_fish_cont_score_preTrt', 'top2atri_preTrt', 'topoihc_preTrt',
+      
+      
+      'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery'
+      
+      
+      
+      
+      'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_numPosLymphNodesRemoved', 'preTrt_posDichLymphNodes'
+      
+      
+      ['tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'tumor_size_cm_preTrt_preSurgeryMin', 'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_numPosLymphNodesRemoved', 'preTrt_posDichLymphNodes', 'tumor_stage_preTrt', 'tumor_stage_preTrtMin', 'tumor_stage_preTrtMax', 'nuclear_grade_preTrt', 'metastasis_stage_preTrt', 'ER_preTrt', 'ER_percentage_preTrt', 'ER_expr_preTrt', 'ER_fmolmg_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt', 'Erbeta_preTrt', 'PR_preTrt', 'PR_percentage_preTrt', 'PR_expr_preTrt', 'PR_fmolmg_preTrt', 'HER2_preTrt', 'HER2_IHC_score_preTrt', 'HER2_expr_preTrt', 'HER2_fish_cont_score_preTrt', 'top2atri_preTrt', 'topoihc_preTrt']
+      
+      'HPV/P16 status',
+      'T-category',
+      'N-category',
+      'N-category_8th_edition',
+      'AJCC 7th edition',
+      'AJCC 8th edition',
+      'Smoking status at Diagnosis (Never/Former/Current)',
+      'Smoking status (Packs/Year)',
+      'Aspiration rate Pre-therapy',
+      'Num Affected Lymph nodes',
+      'R Laterality',
+      'L Laterality',
+      'BOT subsite',
+      'Tonsil subsite',
+      'Soft Palate subsite',
+      'GPS subsite',
+      'White/Caucasian',
+      'Hispanic/Latino',
+      'African American/Black',
+      'Asian',
+      'Native American']
+
+A1 = 'Decision 1 (surgery) Y/N'
+S2 = ['surgery_type (breast preserving/mastectomy )',
                 'Chemo Modification (Y/N)',
                 'Dose modified',
                 'Dose delayed',
@@ -24,7 +64,7 @@ S2 = ['Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)',
                 'SD Primary',
                 'SD Nodal']
 H2 = H1 + [A1] + S2
-A2 = 'Decision 2 Surgery (Y/N)'
+A2 = 'Decision 2 (CC / RT alone)'
 S3 =  ['CC Platinum',
                 'CC Cetuximab',
                 'CC Others',
@@ -43,17 +83,29 @@ S3 =  ['CC Platinum',
                 'DLT_Vascular 2',
                 'DLT_Other 2']
 H3 = H2 + [A2] + S3
-A3 = 'Decision 3 Radiation Therapy (Y/N)'
-Y = ['cens']
+A3 = 'Decision 3 Neck Dissection (Y/N)'
+Y = ['Overall Survival (4 Years)', 'Feeding tube 6m', 'Aspiration rate Post-therapy', 'Dysphagia']
+
+Y: "RFS","Relapse-Free Survival. NOTE: in keeping with 1 as a ""healthy"" status (like pCR) in this database, 1=did NOT relapse (survived/was healthy), and 0=relapsed."
+"RFS_months_or_MIN_months_of_RFS","Months until RFS or minimum months of RFS (in the case that a clinical trial only stated the minimum number of RFS months for all patients)"
+"DFS","Disease-Free Survival. NOTE: in keeping with 1 as a ""healthy"" status (like pCR) in this database, 1=did NOT have disease (survived/was healthy), and 0= did have disease."
+"DFS_months_or_MIN_months_of_DFS","Months until DFS or minimum months of DFS (in the case that a clinical trial only stated the minimum number of DFS months for all patients)"
+"OS","Overall Survival (was the patient alive at the end of the trial?) NOTE: in keeping with 1 as a ""healthy"" status (like pCR) in this database, 1=did survive (survived/was healthy), and 0=did NOT survive."
+"OS_months_or_MIN_months_of_OS","Months of OS or minimum months of OS (in the case that a clinical trial only stated the minimum number of OS months for all patients)"
+"OS_up_until_death","Was the OS variable measured up until a patients' death? 1=YES. 0=NO. 0 indicates that perhaps the trial only followed patients for say 5 or 10 years (see OS_months_or_MIN_months_of_OS for patients in a certain clinical trial to see the maximum length of time a certain trial followed a patient.)"
+
+
+
 decisions = [A1, A2, A3]
 histories = [H1, H2, H3]
 states = [S2, S3, Y]
 action_values = [(-1, 1), (-1, 1), (-1, 1)]
 
 # histories before preprocessing
-H1_prepr = ['Age at Diagnosis (Calculated)',
-            'Pathological Grade',
-            'Gender',
+H1_prepr = ['age',
+            'nuclear_grade_preTrt',
+            'metastasis_stage_preTrt',
+            
             'HPV/P16 status',
             'T-category',
             'N-category',
@@ -98,3 +150,154 @@ S3_prepr =  ['CC Regimen(0= none, 1= platinum based, 2= cetuximab based, 3= othe
                 'SD Nodal 2',
                 'DLT 2']
 H3_prepr = H2_prepr + [A2] + S3_prepr
+
+
+#surgery data breast preserving / mastectomy
+'methotrexate',
+
+
+
+'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'tumor_size_cm_preTrt_preSurgeryMin',
+'months_from_surgery_measured_RCB', 'surgery_type'
+
+#preTrt data
+'biopsy_preTreat'
+'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'tumor_size_cm_preTrt_preSurgeryMin', 'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_numPosLymphNodesRemoved', 'preTrt_posDichLymphNodes', 'tumor_stage_preTrt', 'tumor_stage_preTrtMin', 'tumor_stage_preTrtMax', 'nuclear_grade_preTrt', 'metastasis_stage_preTrt', 'ER_preTrt', 'ER_percentage_preTrt', 'ER_expr_preTrt', 'ER_fmolmg_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt', 'Erbeta_preTrt', 'PR_preTrt', 'PR_percentage_preTrt', 'PR_expr_preTrt', 'PR_fmolmg_preTrt', 'HER2_preTrt', 'HER2_IHC_score_preTrt', 'HER2_expr_preTrt', 'HER2_fish_cont_score_preTrt', 'top2atri_preTrt', 'topoihc_preTrt'
+
+#postTrt data
+'biopsy_postTrt_days', 'pCR_postTrt_days', 'tumor_size_cm_postTrt', 'postTrt_lymph_node_status', 'postTrt_totalLymphNodes', 'postTrt_numPosLymphNodes', 'tumor_stage_postTrt'
+
+#pCR - Pathological complete response
+'pCR_postTrt_days', 'pCR', 'near_pCR', 'pCR_spectrum'
+
+#RCB - Residual Cancer Burden
+'RCB'
+
+#RFS - Relapse-Free Survival
+'RFS'
+ 
+#DFS - Disease-Free Survival
+'DFS'
+
+#OS - Overall Survival
+'OS'
+
+#ER - Estrogen Receptor
+'ER_preTrt', 'ER_percentage_preTrt', 'ER_fmolmg_preTrt', 'estrogen_receptor_blocker_and_stops_production', 'estrogen_receptor_blocker_and_eliminator', 'estrogen_receptor_blocker_and_stops_production',
+'estrogen_receptor_blocker'
+
+
+#ERBB2 - Erb-B2 Receptor Tyrosine Kinase 2
+'ERbb2_preTrt', 'ERBB2_CPN_amplified'
+
+#PR - Progesterone Receptor
+'PR_preTrt', 'PR_percentage_preTrt', 'PR_expr_preTrt', 'PR_fmolmg_preTrt'
+ 
+#HER2 - Human Epidermal Growth Factor Receptor 2
+'HER2_preTrt', 'HER2_IHC_score_preTrt', 'HER2_fish_cont_score_preTrt', 'anti_HER2'
+
+
+#ESR1 - Estrogen Receptor 1
+'ESR1_preTrt'
+
+#Erbeta - Estrogen Receptor Beta
+'Erbeta_preTrt'
+
+#top2atri - Topoisomerase II Alpha
+'top2atri_preTrt', 
+
+#topoihc - Topoisomerase IHC
+'topoihc_preTrt'
+
+#AJCC - American Joint Committee on Cancer
+'clinical_AJCC_stage'
+ 
+#metastasis - Did the tumor metastasize?
+'metastasis', 'metastasis_months'
+
+#dead - Did the patient die?
+'dead', 'died_from_cancer_if_dead'
+
+#biopsy - Did the patient have a biopsy?
+'biopsy_preTreat'
+
+#treatments
+'neoadjuvant_or_adjuvant'
+- this seems to be surgery
+neo=before the main ttt
+adj=after the main ttt
+
+#stand alone ttt
+'gefitinib'
+'fulvestrant'
+
+# pre surgery ttt
+'docetaxel'
+'epirubicin'
+'aromatase_inhibitor'
+'anti_estrogen'
+'anthracycline'
+
+#after surgery ttt
+'anastrozole'
+'capecitabine'
+'docetaxel'
+'epirubicin'
+'tamoxifen'
+'estrogen_receptor_blocker'
+'aromatase_inhibitor'
+'anti_estrogen'
+'anthracycline'
+
+
+#before chemo
+'capecitabine'
+
+#treatment for chemo
+'chemotherapy'
+'taxaneGeneral' 
+'taxane'
+'carboplatin'
+'cetuximab'
+'methotrexate'
+'letrozole'
+'trastuzumab'
+'cyclophosphamide'
+'paclitaxel'
+'fluorouracil'
+'doxorubicin'
+
+# other treatment -> like radiotherapy
+'other',
+'cetuximab'
+'radiotherapyClass'
+
+#no treatment
+'no_treatment', 
+
+# hormone therapy
+'hormone_therapy'
+'hormone_therapyClass'
+
+
+# post menopausal drug
+'postmenopausal_only'
+
+
+#  drug administration
+'oral'
+'intramuscular'
+'intarvenous'
+
+
+#tumor dna index
+'DNA_index'
+
+#tumor chromosome
+'ploidy'
+
+#measurements of tumor
+'S_phase'
+
+# tumor positive?
+'cytokeratin5_pos'
--- processing.py
+++ processing.py
@@ -112,9 +112,223 @@ dlt_dict = {
 treats = [(H1_prepr, H1), (H2_prepr, H2), (H3_prepr, H3)]
 
 # columns to preprocess if the goal of the preprocessing is prediction
-pred = [(H1_prepr + [A1], H1 + [A1]), (H2_prepr + [A2], H2 + [A2]), (H3_prepr + [A3], H3 + [A3])]
 
 
 def rescale(data, data_range, scale_range):
     return ((data - data_range[0]) / (data_range[1] - data_range[0])) * (scale_range[1] - scale_range[0]) + scale_range[0]
+
+
+
+
+
+# ////////
+
+
+# columns for prediction
+
+prediction = [(H1_prepr + [A1], H1 + [A1]), (H2_prepr + [A2], H2 + [A2]), (H3_prepr + [A3], H3 + [A3])]
+
+
+
+
+
+def preprocess(data, prediction_or_treatment, step):
+    if len(data.shape) < 2:
+        data = pd.DataFrame([data], columns=data.index)
+    # dropping unused columns
+    if prediction_or_treatment == 'prediction':
+        data_cleaned = data[prediction[step - 1][0]]
+    else:
+        data_cleaned = data[treats[step - 1][0]]
+    # changing packs/year to numerical
+    data_cleaned.loc[data_cleaned['Smoking status (Packs/Year)'] == '>20', 'Smoking status (Packs/Year)'] = '20'
+    data_cleaned = data_cleaned.astype({'Smoking status (Packs/Year)': 'float64'})
+
+    # removing missing values
+    for c in data_cleaned.columns:
+        if data_cleaned[c].isnull().values.any():
+            data_cleaned[c] = data_cleaned[c].fillna(missing_values[c])
+
+    # preprocessing variables
+    data_cleaned.loc[data_cleaned['Aspiration rate Pre-therapy'] == 'N', 'Aspiration rate Pre-therapy'] = 0
+    data_cleaned.loc[data_cleaned['Aspiration rate Pre-therapy'] == 'Y', 'Aspiration rate Pre-therapy'] = 1
+
+    data_cleaned.loc[data_cleaned['Pathological Grade'] == 'I', 'Pathological Grade'] = 1
+    data_cleaned.loc[data_cleaned['Pathological Grade'] == 'II', 'Pathological Grade'] = 2
+    data_cleaned.loc[data_cleaned['Pathological Grade'] == 'III', 'Pathological Grade'] = 3
+    data_cleaned.loc[data_cleaned['Pathological Grade'] == 'IV', 'Pathological Grade'] = 4
+
+    data_cleaned['R Laterality'] = 0
+    data_cleaned['L Laterality'] = 0
+    data_cleaned.loc[(data_cleaned['Tm Laterality (R/L)'] == 'R') | (
+            data_cleaned['Tm Laterality (R/L)'] == 'Bilateral'), 'R Laterality'] = 1
+    data_cleaned.loc[(data_cleaned['Tm Laterality (R/L)'] == 'L') | (
+            data_cleaned['Tm Laterality (R/L)'] == 'Bilateral'), 'L Laterality'] = 1
+
+    data_cleaned['BOT subsite'] = 0
+    data_cleaned['Tonsil subsite'] = 0
+    data_cleaned['Soft Palate subsite'] = 0
+    data_cleaned['GPS subsite'] = 0
+    data_cleaned.loc[
+        data_cleaned['Tumor subsite (BOT/Tonsil/Soft Palate/Pharyngeal wall/GPS/NOS)'] == 'BOT', 'BOT subsite'] = 1
+    data_cleaned.loc[data_cleaned[
+                         'Tumor subsite (BOT/Tonsil/Soft Palate/Pharyngeal wall/GPS/NOS)'] == 'Tonsil', 'Tonsil subsite'] = 1
+    data_cleaned.loc[data_cleaned[
+                         'Tumor subsite (BOT/Tonsil/Soft Palate/Pharyngeal wall/GPS/NOS)'] == 'Soft palate', 'Soft Palate subsite'] = 1
+    data_cleaned.loc[
+        data_cleaned['Tumor subsite (BOT/Tonsil/Soft Palate/Pharyngeal wall/GPS/NOS)'] == 'GPS', 'GPS subsite'] = 1
+
+    data_cleaned.loc[(data_cleaned['T-category'] == 'Tx') | (data_cleaned['T-category'] == 'Tis'), 'T-category'] = 1
+    data_cleaned.loc[data_cleaned['T-category'] == 'T1', 'T-category'] = 1
+    data_cleaned.loc[data_cleaned['T-category'] == 'T2', 'T-category'] = 2
+    data_cleaned.loc[data_cleaned['T-category'] == 'T3', 'T-category'] = 3
+    data_cleaned.loc[data_cleaned['T-category'] == 'T4', 'T-category'] = 4
+
+    data_cleaned.loc[data_cleaned['N-category'] == 'N0', 'N-category'] = 0
+    data_cleaned.loc[data_cleaned['N-category'] == 'N1', 'N-category'] = 1
+    data_cleaned.loc[data_cleaned['N-category'] == 'N2', 'N-category'] = 2
+    data_cleaned.loc[data_cleaned['N-category'] == 'N3', 'N-category'] = 3
+
+    data_cleaned.loc[data_cleaned['N-category_8th_edition'] == 'N0', 'N-category_8th_edition'] = 0
+    data_cleaned.loc[data_cleaned['N-category_8th_edition'] == 'N1', 'N-category_8th_edition'] = 1
+    data_cleaned.loc[data_cleaned['N-category_8th_edition'] == 'N2', 'N-category_8th_edition'] = 2
+    data_cleaned.loc[data_cleaned['N-category_8th_edition'] == 'N3', 'N-category_8th_edition'] = 3
+
+    data_cleaned.loc[data_cleaned['AJCC 7th edition'] == 'I', 'AJCC 7th edition'] = 1
+    data_cleaned.loc[data_cleaned['AJCC 7th edition'] == 'II', 'AJCC 7th edition'] = 2
+    data_cleaned.loc[data_cleaned['AJCC 7th edition'] == 'III', 'AJCC 7th edition'] = 3
+    data_cleaned.loc[data_cleaned['AJCC 7th edition'] == 'IV', 'AJCC 7th edition'] = 4
+
+    data_cleaned.loc[data_cleaned['AJCC 8th edition'] == 'I', 'AJCC 8th edition'] = 1
+    data_cleaned.loc[data_cleaned['AJCC 8th edition'] == 'II', 'AJCC 8th edition'] = 2
+    data_cleaned.loc[data_cleaned['AJCC 8th edition'] == 'III', 'AJCC 8th edition'] = 3
+    data_cleaned.loc[data_cleaned['AJCC 8th edition'] == 'IV', 'AJCC 8th edition'] = 4
+
+    data_cleaned.loc[data_cleaned['Gender'] == 'Male', 'Gender'] = 0
+    data_cleaned.loc[data_cleaned['Gender'] == 'Female', 'Gender'] = 1
+
+    data_cleaned['White/Caucasian'] = 0
+    data_cleaned['Hispanic/Latino'] = 0
+    data_cleaned['African American/Black'] = 0
+    data_cleaned['Asian'] = 0
+    data_cleaned['Native American'] = 0
+    data_cleaned.loc[data_cleaned['Race'] == 'White/Caucasion', 'White/Caucasian'] = 1
+    data_cleaned.loc[data_cleaned['Race'] == 'Hispanic/Latino', 'Hispanic/Latino'] = 1
+    data_cleaned.loc[data_cleaned['Race'] == 'African American/Black', 'African American/Black'] = 1
+    data_cleaned.loc[data_cleaned['Race'] == 'Asian', 'Asian'] = 1
+    data_cleaned.loc[data_cleaned['Race'] == 'Native American', 'Native American'] = 1
+
+    data_cleaned.loc[data_cleaned['HPV/P16 status'] == 'Positive', 'HPV/P16 status'] = 1
+    data_cleaned.loc[data_cleaned['HPV/P16 status'] == 'Negative', 'HPV/P16 status'] = -1
+    data_cleaned.loc[data_cleaned['HPV/P16 status'] == 'Unknown', 'HPV/P16 status'] = 0
+
+    data_cleaned.loc[data_cleaned[
+                         'Smoking status at Diagnosis (Never/Former/Current)'] == 'Formar', 'Smoking status at Diagnosis (Never/Former/Current)'] = 1
+    data_cleaned.loc[data_cleaned[
+                         'Smoking status at Diagnosis (Never/Former/Current)'] == 'Current', 'Smoking status at Diagnosis (Never/Former/Current)'] = 2
+    data_cleaned.loc[data_cleaned[
+                         'Smoking status at Diagnosis (Never/Former/Current)'] == 'Never', 'Smoking status at Diagnosis (Never/Former/Current)'] = 0
+
+    data_cleaned["Num Affected Lymph nodes"] = data_cleaned["Affected Lymph node"].apply(lambda x: len(x.split(',')))
+
+    if (prediction_or_treatment == 'pred') or (step > 1):
+        data_cleaned.loc[
+            data_cleaned['Decision 1 (Induction Chemo) Y/N'] == 'N', 'Decision 1 (Induction Chemo) Y/N'] = 0
+        data_cleaned.loc[
+            data_cleaned['Decision 1 (Induction Chemo) Y/N'] == 'Y', 'Decision 1 (Induction Chemo) Y/N'] = 1
+
+    if step > 1:
+        data_cleaned.loc[(data_cleaned['Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] == 'None') | (
+                data_cleaned[
+                    'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] == 'NOS'), 'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] = 0
+        data_cleaned.loc[data_cleaned[
+                             'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] == 'Single', 'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] = 1
+        data_cleaned.loc[data_cleaned[
+                             'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] == 'Doublet', 'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] = 2
+        data_cleaned.loc[data_cleaned[
+                             'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] == 'Triplet', 'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] = 3
+        data_cleaned.loc[data_cleaned[
+                             'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] == 'Quadruplet', 'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'] = 4
+
+        data_cleaned.loc[data_cleaned['Chemo Modification (Y/N)'] == 'N', 'Chemo Modification (Y/N)'] = 0
+        data_cleaned.loc[data_cleaned['Chemo Modification (Y/N)'] == 'Y', 'Chemo Modification (Y/N)'] = 1
+
+        data_cleaned['Dose modified'] = 0
+        data_cleaned['Dose delayed'] = 0
+        data_cleaned['Dose cancelled'] = 0
+        data_cleaned['Regimen modification'] = 0
+        data_cleaned.loc[(data_cleaned[
+                              'Modification Type (0= no dose adjustment, 1=dose modified, 2=dose delayed, 3=dose cancelled, 4=dose delayed & modified, 5=regimen modification, 9=unknown)'] == 1) | (
+                                 data_cleaned[
+                                     'Modification Type (0= no dose adjustment, 1=dose modified, 2=dose delayed, 3=dose cancelled, 4=dose delayed & modified, 5=regimen modification, 9=unknown)'] == 4), 'Dose modified'] = 1
+        data_cleaned.loc[(data_cleaned[
+                              'Modification Type (0= no dose adjustment, 1=dose modified, 2=dose delayed, 3=dose cancelled, 4=dose delayed & modified, 5=regimen modification, 9=unknown)'] == 2) | (
+                                 data_cleaned[
+                                     'Modification Type (0= no dose adjustment, 1=dose modified, 2=dose delayed, 3=dose cancelled, 4=dose delayed & modified, 5=regimen modification, 9=unknown)'] == 4), 'Dose delayed'] = 1
+        data_cleaned.loc[data_cleaned[
+                             'Modification Type (0= no dose adjustment, 1=dose modified, 2=dose delayed, 3=dose cancelled, 4=dose delayed & modified, 5=regimen modification, 9=unknown)'] == 3, 'Dose cancelled'] = 1
+        data_cleaned.loc[data_cleaned[
+                             'Modification Type (0= no dose adjustment, 1=dose modified, 2=dose delayed, 3=dose cancelled, 4=dose delayed & modified, 5=regimen modification, 9=unknown)'] == 5, 'Regimen modification'] = 1
+
+        data_cleaned.loc[data_cleaned['DLT (Y/N)'] == 'N', 'DLT (Y/N)'] = 0
+        data_cleaned.loc[data_cleaned['DLT (Y/N)'] == 'Y', 'DLT (Y/N)'] = 1
+
+        data_cleaned['DLT_Other'] = 0
+        for index, row in data_cleaned.iterrows():
+            if row['DLT_Type'] == 'None':
+                continue
+            for i in re.split('&|and|,', row['DLT_Type']):
+                if i.strip() != '' and data_cleaned.loc[index, dlt_dict[i.strip()]] == 0:
+                    data_cleaned.loc[index, dlt_dict[i.strip()]] = 1
+
+        if (prediction_or_treatment == 'pred') or (step > 2):
+            data_cleaned.loc[data_cleaned['Decision 2 (CC / RT alone)'] == 'RT alone', 'Decision 2 (CC / RT alone)'] = 0
+            data_cleaned.loc[data_cleaned['Decision 2 (CC / RT alone)'] == 'CC', 'Decision 2 (CC / RT alone)'] = 1
+
+    if step > 2:
+        data_cleaned['CC Platinum'] = 0
+        data_cleaned['CC Cetuximab'] = 0
+        data_cleaned['CC Others'] = 0
+        data_cleaned.loc[data_cleaned[
+                             'CC Regimen(0= none, 1= platinum based, 2= cetuximab based, 3= others, 9=unknown)'] == 1, 'CC Platinum'] = 1
+        data_cleaned.loc[data_cleaned[
+                             'CC Regimen(0= none, 1= platinum based, 2= cetuximab based, 3= others, 9=unknown)'] == 2, 'CC Cetuximab'] = 1
+        data_cleaned.loc[data_cleaned[
+                             'CC Regimen(0= none, 1= platinum based, 2= cetuximab based, 3= others, 9=unknown)'] == 3, 'CC Others'] = 1
+
+        data_cleaned.loc[data_cleaned['CC modification (Y/N)'] == 'N', 'CC modification (Y/N)'] = 0
+        data_cleaned.loc[data_cleaned['CC modification (Y/N)'] == 'Y', 'CC modification (Y/N)'] = 1
+
+        data_cleaned['DLT_Dermatological 2'] = 0
+        data_cleaned['DLT_Neurological 2'] = 0
+        data_cleaned['DLT_Gastrointestinal 2'] = 0
+        data_cleaned['DLT_Hematological 2'] = 0
+        data_cleaned['DLT_Nephrological 2'] = 0
+        data_cleaned['DLT_Vascular 2'] = 0
+        data_cleaned['DLT_Infection (Pneumonia) 2'] = 0
+        data_cleaned['DLT_Other 2'] = 0
+        for index, row in data_cleaned.iterrows():
+            if row['DLT 2'] == 'None':
+                continue
+            for i in re.split('&|and|,', row['DLT 2']):
+                if i.strip() != '':
+                    data_cleaned.loc[index, dlt_dict[i.strip()] + ' 2'] = 1
+
+        if (prediction_or_treatment == 'pred') or (step > 3):
+            data_cleaned.loc[
+                data_cleaned['Decision 3 Neck Dissection (Y/N)'] == 'N', 'Decision 3 Neck Dissection (Y/N)'] = 0
+            data_cleaned.loc[
+                data_cleaned['Decision 3 Neck Dissection (Y/N)'] == 'Y', 'Decision 3 Neck Dissection (Y/N)'] = 1
+
+    # selecting needed columns
+    if prediction_or_treatment == 'pred':
+        data_cleaned = data_cleaned[pred[step - 1][1]]
+    else:
+        data_cleaned = data_cleaned[treats[step - 1][1]]
+
+    # scaling data to [-1, 1] range
+    for c in data_cleaned.columns:
+        data_cleaned[c] = rescale(data_cleaned[c], feature_range[c], (-1, 1))
+
+    return data_cleaned
 
