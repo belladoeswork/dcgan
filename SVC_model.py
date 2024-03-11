4582aaecd44a74e9534c21c1e441d6fd46553be3
belladoeswork <belladoeswork@gmail.com>
2024-03-12 05:41:05 +0800

    some other changes

--- SVC_model.py
+++ SVC_model.py
@@ -7,49 +7,21 @@ from processing import *
 s = './Patient twin/'
 # dictionary of files containing transition models
 trans_files = {
-    'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)': 'svm_prescribed_chemo_poly.joblib',
-    'Chemo Modification (Y/N)': 'svm_chemo_modification_rbf.joblib',
-    'Dose modified': 'svm_dose_modified_poly.joblib',
-    'Dose delayed': 'svm_dose_delayed_rbf.joblib',
-    'Dose cancelled': 'svm_dose_cancelled_rbf.joblib',
-    'Regimen modification': 'svm_regimen_modification_poly.joblib',
-    'DLT (Y/N)': 'svm_dlt_rbf.joblib',
-    'DLT_Dermatological': 'svm_dlt_dermatological_poly.joblib',
-    'DLT_Neurological': 'svm_dlt_neurological_rbf.joblib',
-    'DLT_Gastrointestinal': 'svm_dlt_gastrointestinal_poly.joblib',
-    'DLT_Hematological': 'svm_dlt_hematological_rbf.joblib',
-    'DLT_Nephrological': 'svm_dlt_nephrological_poly.joblib',
-    'DLT_Vascular': 'svm_dlt_vascular_rbf.joblib',
-    'DLT_Infection (Pneumonia)': 'svm_dlt_infection_poly.joblib',
-    'DLT_Other': 'svm_dlt_other_rbf.joblib',
-    'DLT_Grade': 'svm_dlt_grade_poly.joblib',
-    'No imaging (0=N, 1=Y)': 'svm_no_imaging_rbf.joblib',
-    'CR Primary': 'svm_cr_primary_rbf.joblib',
-    'CR Nodal': 'svm_cr_nodal_poly.joblib',
-    'PR Primary': 'svm_pr_primary_rbf.joblib',
-    'PR Nodal': 'svm_pr_nodal_rbf.joblib',
-    'SD Primary': 'svm_sd_primary_rbf.joblib',
-    'SD Nodal': 'svm_sd_nodal_poly.joblib',
-    'CC Platinum': 'svm_cc_regimen_rbf.joblib',
-    'CC Cetuximab': 'svm_cc_regimen_rbf.joblib',
-    'CC Others': 'svm_cc_regimen_rbf.joblib',
-    'CC modification (Y/N)': 'svm_cc_modification_rbf.joblib',
-    'CR Primary 2': 'svm_cr_primary_2_poly.joblib',
-    'CR Nodal 2': 'svm_cr_nodal_2_sig.joblib',
-    'PR Primary 2': 'svm_pr_primary_2_poly.joblib',
-    'PR Nodal 2': 'svm_pr_nodal_2_poly.joblib',
-    'SD Primary 2': 'svm_sd_primary_2_rbf.joblib',
-    'SD Nodal 2': 'svm_sd_nodal_2_rbf.joblib',
-    'DLT_Dermatological 2': 'svm_dlt_dermatological_2_rbf.joblib',
-    'DLT_Neurological 2': 'svm_dlt_neurological_2_poly.joblib',
-    'DLT_Gastrointestinal 2': 'svm_dlt_gastrointestinal_2_poly.joblib',
-    'DLT_Hematological 2': 'svm_dlt_hematological_2_poly.joblib',
-    'DLT_Nephrological 2': 'svm_dlt_nephrological_2_poly.joblib',
-    'DLT_Vascular 2': 'svm_dlt_vascular_2_rbf.joblib',
-    'DLT_Other 2': 'svm_dlt_other_2_poly.joblib',
-    'Overall Survival (4 Years)': 'svm_overall_survival_4_years_rbf.joblib',
-    'Feeding tube 6m': 'svm_feeding_tube_rbf.joblib',
-    'Aspiration rate Post-therapy': 'svm_aspiration_rate_rbf.joblib'
+    'postTrt_numPosLymphNodes': 'svm_postTrt_numPosLymphNodes_.joblib', 
+    'metastasis_months': 'svm_metastasis_months_.joblib', 
+    'tumor_size_cm_postTrt': 'svm_tumor_size_cm_postTrt_.joblib',
+    'metastasis_months_1': 'svm_metastasis_months_1_.joblib', 
+    'pCR': 'svm_pCR_.joblib', 
+    'near_pCR': 'svm_near_pCR_.joblib', 
+    'metastasis_1': 'svm_metastasis_1_.joblib', 
+    'taxane': 'svm_taxane_.joblib',
+    'metastasis_months_2': 'svm_metastasis_months_2_.joblib', 
+    'estrogen_receptor': 'svm_estrogen_receptor_.joblib', 
+    'letrozole': 'svm_letrozole_.joblib', 
+    'aromatase_inhibitor': 'svm_aromatase_inhibitor_.joblib', 
+    'tamoxifen': 'svm_tamoxifen_.joblib', 
+    'anti_estrogen': 'svm_anti_estrogen_.joblib', 
+    'other_treatment': 'svm_other_treatment_.joblib'
 }
 
 # outcome encodings in the transition models
@@ -112,41 +84,31 @@ outcome_dicts = {'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)'
 
 default_outcomes = {
     A1: {
-        'Prescribed Chemo (Single/doublet/triplet/quadruplet/none/NOS)': -1.0,
-        'Chemo Modification (Y/N)': -1.0,
-        'Dose modified': -1.0,
-        'Dose delayed': -1.0,
-        'Dose cancelled': -1.0,
-        'Regimen modification': -1.0,
-        'DLT (Y/N)': -1.0,
-        'DLT_Dermatological': -1.0,
-        'DLT_Neurological': -1.0,
-        'DLT_Gastrointestinal': -1.0,
-        'DLT_Hematological': -1.0,
-        'DLT_Nephrological': -1.0,
-        'DLT_Vascular': -1.0,
-        'DLT_Infection (Pneumonia)': -1.0,
-        'DLT_Other': -1.0,
-        'DLT_Grade': -1.0
+        'postTrt_numPosLymphNodes':0, 
+        'metastasis_months':0, 
+        'tumor_size_cm_postTrt':0
     },
     A2: {
-        'CC Platinum': -1.0,
-        'CC Cetuximab': -1.0,
-        'CC Others': -1.0,
-        'DLT_Dermatological 2': -1.0,
-        'DLT_Neurological 2': -1.0,
-        'DLT_Gastrointestinal 2': -1.0,
-        'DLT_Hematological 2': -1.0,
-        'DLT_Nephrological 2': -1.0,
-        'DLT_Vascular 2': -1.0,
-        'DLT_Other 2': -1.0
+        'metastasis_months_1':0 , 
+        'pCR': 0, 
+        'near_pCR': 0, 
+        'metastasis_1': 0, 
+        'taxane':0
     },
-    A3: {}
+    A3: {'metastasis_months_2': 0, 
+        'estrogen_receptor': 0, 
+        'letrozole': 0,
+        'aromatase_inhibitor': 0, 
+        'tamoxifen': 0, 
+        'anti_estrogen': 0, 
+        'other_treatment': 0
+    },
+    A4: {}
 }
 
-derived_outcomes = {
-    'Dysphagia': (lambda p: max(p['Feeding tube 6m'], p['Aspiration rate Post-therapy']))
-}
+# derived_outcomes = {
+#     'Dysphagia': (lambda p: max(p['Feeding tube 6m'], p['Aspiration rate Post-therapy']))
+# }
 
 
 def load_patient_twin():
@@ -183,8 +145,8 @@ def next_state(history, action, action_value, outcomes, trans_model):
     for o in outcomes:
         if (o in default_outcomes[action].keys()) and (action_value == -1):
             y.loc[o] = default_outcomes[action][o]
-        elif o in derived_outcomes.keys():
-            y.loc[o] = derived_outcomes[o](y)
+        # elif o in derived_outcomes.keys():
+        #     y.loc[o] = derived_outcomes[o](y)
         else:
             y.loc[o] = compute_outcome(history, action_value, trans_model[o], outcome_dicts[o])
     return y
--- histories.py
+++ histories.py
@@ -4,53 +4,79 @@
 # Y= Outcome
 
 
-'biopsy_preTreat', 'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_numPosLymphNodesRemoved', 'preTrt_posDichLymphNodes', 'tumor_stage_preTrt', 'nuclear_grade_preTrt', 'ER_preTrt', 'PR_preTrt', 'HER2_preTrt', 'ER_fmolmg_preTrt', 'Erbeta_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt', 'ERBB2_CPN_amplified', 'PR_percentage_preTrt', 'PR_fmolmg_preTrt', 'HER2_IHC_score_preTrt', 'age', 'race', 'menopausal_status', 'HER2_fish_cont_score_preTrt', 'pam50', 'hist_grade', 'cytokeratin5_pos', 'ploidy', 'S_phase', 'DNA_index', 'menopausal_status', 'Clinical AJCC Stage', 'top2atri_preTrt', 'topoihc_preTrt', 'surgery', 'tumor_stage_postTrt', 'tumor_size_cm_postTrt', 'postTrt_numPosLymphNodes', 'postTrt_lymph_node_status', 'postTrt_totalLymphNodes', 'postTrt_numPosLymphNodes', 'RCB', 'other_treatment', 'metastasis', 'metastasis_months', 'RFS', 'DFS', 'chemotherapy', 'anthracycline', 'taxane', 'doxorubicin', 'epirubicin', 'docetaxel', 'capecitabine', 'fluorouracil', 'paclitaxel', 'cyclophosphamide', 'methotrexate', 'carboplatin', 'near_pCR', 'pCR', 'cetuximab', 'gefitinib', 'tamoxifen', 'aromatase_inhibitor', 'fulvestrant', 'anastrozole', 'letrozole', 'anti_estrogen', 'estrogen_receptor_blocker', 'estrogen_receptor_blocker_and_stops_production', 'estrogen_receptor_blocker_and_eliminator', 'anti_HER2', 'radiotherapy', 'trastuzumab', 'no_treatment', 'OS', 'died_from_cancer_if_dead'
+# 'biopsy_preTreat', 'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_numPosLymphNodesRemoved', 'preTrt_posDichLymphNodes', 'tumor_stage_preTrt', 'nuclear_grade_preTrt', 'ER_preTrt', 'PR_preTrt', 'HER2_preTrt', 'ER_fmolmg_preTrt', 'Erbeta_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt', 'ERBB2_CPN_amplified', 'PR_percentage_preTrt', 'PR_fmolmg_preTrt', 'HER2_IHC_score_preTrt', 'age', 'race', 'menopausal_status', 'HER2_fish_cont_score_preTrt', 'pam50', 'hist_grade', 'cytokeratin5_pos', 'ploidy', 'S_phase', 'DNA_index', 'menopausal_status', 'Clinical AJCC Stage', 'top2atri_preTrt', 'topoihc_preTrt', 'surgery', 'tumor_stage_postTrt', 'tumor_size_cm_postTrt', 'postTrt_lymph_node_status', 'postTrt_numPosLymphNodes', 'RCB', 'other_treatment', 'metastasis', 'metastasis_months', 'RFS', 'DFS', 'chemotherapy', 'anthracycline', 'taxane', 'doxorubicin', 'epirubicin', 'docetaxel', 'capecitabine', 'fluorouracil', 'paclitaxel', 'cyclophosphamide', 'methotrexate', 'carboplatin', 'near_pCR', 'pCR', 'cetuximab', 'gefitinib', 'tamoxifen', 'aromatase_inhibitor', 'fulvestrant', 'anastrozole', 'letrozole', 'anti_estrogen', 'estrogen_receptor_blocker', 'estrogen_receptor_blocker_and_stops_production', 'estrogen_receptor_blocker_and_eliminator', 'anti_HER2', 'radiotherapy', 'trastuzumab', 'OS', 'died_from_cancer_if_dead'
 
 
 
 
 
-H1 = ['biopsy_preTreat', 
-      'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 
-      'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_posDichLymphNodes', 'tumor_stage_preTrt', 'nuclear_grade_preTrt', 'ER_preTrt', 'PR_preTrt', 'HER2_preTrt', 'ER_fmolmg_preTrt', 'Erbeta_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt',  'ERBB2_CPN_amplified', 'PR_percentage_preTrt', 'PR_fmolmg_preTrt', 'HER2_IHC_score_preTrt',
-      'age', 'Race', 'menopausal_status', 'HER2_fish_cont_score_preTrt',
-      'pam50', 'hist_grade', 'cytokeratin5_pos', 'ploidy', 'S_phase', 'top2atri_preTrt', 'topoihc_preTrt'
-]
+# H1 = ['biopsy_preTreat', 
+#       'tumor_size_cm_preTrt_preSurgery', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 
+#       'preTrt_lymph_node_status', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'preTrt_posDichLymphNodes', 'tumor_stage_preTrt', 'nuclear_grade_preTrt', 'ER_preTrt', 'PR_preTrt', 'HER2_preTrt', 'ER_fmolmg_preTrt', 'Erbeta_preTrt', 'ESR1_preTrt', 'ERbb2_preTrt',  'ERBB2_CPN_amplified', 'PR_percentage_preTrt', 'PR_fmolmg_preTrt', 'HER2_IHC_score_preTrt',
+#       'age', 'Race', 'menopausal_status', 'HER2_fish_cont_score_preTrt',
+#       'pam50', 'hist_grade', 'cytokeratin5_pos', 'ploidy', 'S_phase', 'top2atri_preTrt', 'topoihc_preTrt', 'trastuzumab' 
+# ]
+
+
+#variane
+H1 = ['ER_fmolmg_preTrt', 'PR_fmolmg_preTrt', 'age', 'S_phase', 'topoihc_preTrt', 'ploidy', 'PR_percentage_preTrt', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'tumor_size_cm_preTrt_preSurgery', 'HER2_fish_cont_score_preTrt', 'preTrt_lymph_node_status', 'preTrt_posDichLymphNodes', 'hist_grade', 'nuclear_grade_preTrt', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'PR_preTrt', 'ESR1_preTrt', 'HER2_preTrt', 'tumor_stage_preTrt']
+
+#cross validation
+# H1 = ['ER_fmolmg_preTrt', 'PR_fmolmg_preTrt', 'age', 'S_phase', 'topoihc_preTrt', 'ploidy', 'PR_percentage_preTrt', 'preTrt_totalLymphNodes', 'preTrt_numPosLymphNodes', 'tumor_size_cm_preTrt_preSurgery', 'HER2_fish_cont_score_preTrt', 'preTrt_lymph_node_status', 'preTrt_posDichLymphNodes', 'hist_grade', 'nuclear_grade_preTrt', 'tumor_size_cm_secondAxis_preTrt_preSurgery', 'PR_preTrt', 'ESR1_preTrt', 'HER2_preTrt', 'tumor_stage_preTrt', 'ER_preTrt', 'top2atri_preTrt', 'ERBB2_CPN_amplified', 'cytokeratin5_pos', 'Erbeta_preTrt', 'pam50', 'ERbb2_preTrt', 'HER2_IHC_score_preTrt', 'menopausal_status', 'trastuzumab', 'Race', 'biopsy_preTreat']
+
+
 
 A1 = 'surgery'
-S2 = ['tumor_stage_postTrt', 'tumor_size_cm_postTrt',  'postTrt_numPosLymphNodes',  'postTrt_lymph_node_status', 'postTrt_totalLymphNodes', 'RCB', 'other_treatment', 'metastasis', 'metastasis_months', 'RFS', 'DFS']
+# S2 = ['tumor_stage_postTrt', 'tumor_size_cm_postTrt',  'postTrt_numPosLymphNodes',  'postTrt_lymph_node_status', 'RCB', 'other_treatment', 'metastasis', 'metastasis_months', 'trastuzumab']
 
+#variance
+S2 = ['postTrt_numPosLymphNodes', 'metastasis_months', 'tumor_size_cm_postTrt']
+
+#cross validation
+# S2 = ['postTrt_numPosLymphNodes']
 
 H2 = H1 + [A1] + S2
 
 A2 = 'chemotherapy'
-S3 =  ['anthracycline', 'taxane', 'RFS', 'DFS',
-'doxorubicin', 'epirubicin', 'docetaxel', 'capecitabine', 'fluorouracil', 'paclitaxel', 'cyclophosphamide', 'methotrexate', 'carboplatin', 'near_pCR', 'pCR', 'metastasis', 'metastasis_months', 'cetuximab', 'gefitinib', 'other_treatment' ]
+# S3 =  ['anthracycline', 'taxane',
+# 'doxorubicin', 'epirubicin', 'docetaxel', 'capecitabine', 'fluorouracil', 'paclitaxel', 'cyclophosphamide', 'methotrexate', 'carboplatin', 'near_pCR', 'pCR', 'metastasis_1', 'metastasis_months_1', 'cetuximab', 'gefitinib', 'other_treatment' ]
 
+
+#variance
+S3 = ['metastasis_months_1', 'pCR', 'near_pCR', 'metastasis_1', 'taxane']
+
+#cross validation
+# S3 = ['metastasis_months_1']
 
 H3 = H2 + [A2] + S3
 
 A3 = 'hormone_therapy'
-S4 = ['tamoxifen', 'aromatase_inhibitor',  'fulvestrant', 'anastrozole','letrozole', 'anti_estrogen', 
-      'estrogen_receptor_blocker', 'estrogen_receptor_blocker_and_stops_production', 'estrogen_receptor_blocker_and_eliminator', 'anti_HER2', 'other_treatment', 'metastasis', 'metastasis_months', 'RFS', 'DFS'
-    ]
+# S4 = ['tamoxifen', 'aromatase_inhibitor',  'fulvestrant', 'anastrozole','letrozole', 'anti_estrogen', 'estrogen_receptor', 'anti_HER2', 'other_treatment', 'metastasis_2', 'metastasis_months_2'
+#     ]
+
+
+#variance
+S4 = ['metastasis_months_2', 'estrogen_receptor', 'letrozole', 'aromatase_inhibitor', 'tamoxifen', 'anti_estrogen', 'other_treatment']
+
+# cross validation
+# S4 = ['metastasis_months_2', 'estrogen_receptor', 'letrozole']
 
 H4 = H3 + [A3] + S4
 
 A4 = 'radiotherapy'
-S5 = ['trastuzumab', 'metastasis', 'metastasis_months', 'other_treatment', 'RFS', 'DFS']
+# S5 = ['trastuzumab', 'metastasis', 'metastasis_months', 'other_treatment', 'RFS', 'DFS']
     
 
-H5 = H4 + [A4] + S5
+# H5 = H4 + [A4] + S5
 
-A5 = 'no_treatment'
+# A5 = 'no_treatment'
 # S6 = ['tumor_size_cm_postTrt', 'RFS', 'DFS']
     
-Y = [ 'OS','died_from_cancer_if_dead']
+Y = [ 'OS','died_from_cancer_if_dead', 'RFS', 'DFS']
 
-decisions = [A1, A2, A3, A4, A5]
-histories = [H1, H2, H3, H4, H5 ]
-states = [S2, S3, S4, S5, Y]
+decisions = [A1, A2, A3, A4]
+histories = [H1, H2, H3, H4 ]
+states = [S2, S3, S4, Y]
 
 action_values = [(-1, 1), (-1, 1), (-1, 1)]
 
